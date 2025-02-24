b30a7f878eabe469605398000fac436141fd1272
lazarus kipchumba <lazaruskipchumba6@gmail.com>
2025-02-24 13:01:33 +0300

    pizza

--- server/app.py
+++ server/app.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-from models import db, Restaurant, RestaurantPizza, Pizza
+from flask import Flask, request, make_response, jsonify
 from flask_migrate import Migrate
-from flask import Flask, request, make_response
 from flask_restful import Api, Resource
+from models import db, Restaurant, Pizza, RestaurantPizza
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -19,11 +19,69 @@ db.init_app(app)
 
 api = Api(app)
 
-
 @app.route("/")
 def index():
     return "<h1>Code challenge</h1>"
 
+class Restaurants(Resource):
+    def get(self):
+        restaurants = Restaurant.query.all()
+        # Use the default serialization rules (exclude restaurant_pizzas)
+        return make_response([restaurant.to_dict() for restaurant in restaurants], 200)
+
+class RestaurantById(Resource):
+    def get(self, id):
+        restaurant = Restaurant.query.get(id)
+        if restaurant:
+            # Manually include restaurant_pizzas and associated pizza data
+            restaurant_data = restaurant.to_dict()
+            restaurant_data['restaurant_pizzas'] = [
+                {
+                    "id": rp.id,
+                    "pizza": rp.pizza.to_dict(),
+                    "pizza_id": rp.pizza_id,
+                    "price": rp.price,
+                    "restaurant_id": rp.restaurant_id
+                }
+                for rp in restaurant.restaurant_pizzas
+            ]
+            return make_response(restaurant_data, 200)
+        else:
+            return make_response({"error": "Restaurant not found"}, 404)
+
+    def delete(self, id):
+        restaurant = Restaurant.query.get(id)
+        if restaurant:
+            db.session.delete(restaurant)
+            db.session.commit()
+            return make_response('', 204)
+        else:
+            return make_response({"error": "Restaurant not found"}, 404)
+
+class Pizzas(Resource):
+    def get(self):
+        pizzas = Pizza.query.all()
+        return make_response([pizza.to_dict() for pizza in pizzas], 200)
+
+class RestaurantPizzas(Resource):
+    def post(self):
+        data = request.get_json()
+        try:
+            restaurant_pizza = RestaurantPizza(
+                price=data['price'],
+                pizza_id=data['pizza_id'],
+                restaurant_id=data['restaurant_id']
+            )
+            db.session.add(restaurant_pizza)
+            db.session.commit()
+            return make_response(restaurant_pizza.to_dict(), 201)
+        except ValueError as e:
+            return make_response({"errors": [str(e)]}, 400)
+
+api.add_resource(Restaurants, '/restaurants')
+api.add_resource(RestaurantById, '/restaurants/<int:id>')
+api.add_resource(Pizzas, '/pizzas')
+api.add_resource(RestaurantPizzas, '/restaurant_pizzas')
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
